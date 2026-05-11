174162246edeb1bae57c49f36bc0d901ee41d69d
Franco <franco.montenegro@usach.cl>
2026-05-10 22:13:32 -0400

    Feat: user service layer

--- src/main/java/mingeso/first/travelAgencyBackend/services/UserService.java
+++ src/main/java/mingeso/first/travelAgencyBackend/services/UserService.java
@@ -3,14 +3,11 @@ package mingeso.first.travelAgencyBackend.services;
 import mingeso.first.travelAgencyBackend.entities.UserEntity;
 import mingeso.first.travelAgencyBackend.enums.AccountStatus;
 import mingeso.first.travelAgencyBackend.repositories.UserRepository;
-
 import mingeso.first.travelAgencyBackend.exceptions.BadRequestException;
 
 import static mingeso.first.travelAgencyBackend.utils.ValidationUtils.isValidEmail;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -26,7 +23,7 @@ public class UserService {
     }
 
     public UserEntity registerUser(UserEntity user) {
-        if (user.getName() == null || user.getEmail() == null || user.getPassword() == null) {
+        if (user.getName() == null || user.getLastName() == null || user.getEmail() == null || user.getPassword() == null) {
             throw new BadRequestException("Missing required fields");
         }
         if (!isValidEmail(user.getEmail())) {
@@ -35,9 +32,26 @@ public class UserService {
         if (userRepository.findByEmail(user.getEmail()).isPresent()) {
             throw new BadRequestException("Email already in use");
         }
-        user.setStateAccount(AccountStatus.valueOf("ACTIVE"));
+        user.setStateAccount(AccountStatus.ACTIVE);
 
         return userRepository.save(user);
+    }
+
+    public UserEntity updateUserDetails(Long id, UserEntity updatedData) {
+        UserEntity existingUser = userRepository.findById(id)
+                .orElseThrow(() -> new BadRequestException("User not found"));
+        existingUser.setName(updatedData.getName());
+        existingUser.setPhone(updatedData.getPhone());
+        existingUser.setNationality(updatedData.getNationality());
+
+        return userRepository.save(existingUser);
+    }
+
+    public void deleteUser(Long id) {
+        UserEntity user = userRepository.findById(id)
+                .orElseThrow(() -> new BadRequestException("User not found"));
+        user.setStateAccount(AccountStatus.INACTIVE);
+        userRepository.save(user);
     }
 
 }
